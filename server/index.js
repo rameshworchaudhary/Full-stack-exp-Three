3ed8264c107892753e43026b9c4d12b93f928cb0
Rameshwor Chaudhary <chaudharyishwor143@gmail.com>
2026-08-04 16:11:32 +0530

    Fix Vercel Express API

--- server/index.js
+++ server/index.js
@@ -18,8 +18,4 @@ app.get("/", (req, res) => {
   res.send("JWT Authentication API Running...");
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
